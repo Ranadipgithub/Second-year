23f3718ab8dd30be2557a0f9e32ef382123a160b
Rana Dutta <duttarana189@gmail.com>
2024-07-19 21:41:52 +0530

    Initial commit

---
+++ secondYear.js
@@ -0,0 +1,148 @@
+export const secondYear = [
+  {
+    image: "./Members/Abhilasha.jpg",
+    name: "Abhilasha Roy",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/abhilasha-roy-11353829a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/abhi_mriga?utm_source=qr&igsh=dXlqYjQwbzY2eWRr",
+  },
+  {
+    image: "./Members/Amar.jpg",
+    name: "Amar Kumar Barnwal",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/amar-kumar-barnwal-undefined-573b33304?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/igneelgojo845?igsh=dmpuY2FqdnVuNW5x",
+  },
+  {
+    image: "./Members/Ankit.jpg",
+    name: "Ankit Kumar",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/ankitkumar9731?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/ankitkumar9731?utm_source=qr&igsh=MWMzdmg2cjRhZnN3aQ==",
+  },
+  {
+    image: "./Members/Ankita.jpg",
+    name: "Ankita Mukherjee",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/ankita-mukherjee-9287832aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/_ankita0906_/?utm_source=qr&igsh=OXNxZnVmYW1ycXRo",
+  },
+  {
+    image: "./Members/Ashar.jpg",
+    name: "Ashar Alam",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/ashar-alam-302928284?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/asharxefsane?utm_source=qr&igsh=MXhlbzNoc21leWlnaQ==",
+  },
+  {
+    image: "./Members/Divyam.jpg",
+    name: "Divyam Sharma",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/divyam-sharma-378279220?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/divyam.sharma.589100?utm_source=qr&igsh=eXI2Z2sydzh2YXJ2",
+  },
+  {
+    image: "./Members/Hardik.jpg",
+    name: "Hardik Lavania",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/hardik-lavania-467248288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/hardik25_5?igsh=YWtnbTk2YXRvbXc0",
+  },
+  {
+    image: "./Members/Mahima.jpg",
+    name: "Mahima Singvi",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/mahima-singhvi-193217293?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    instagram:
+      "https://www.instagram.com/_mahima.singhvi_?igsh=MXVwZ255dDNnOHd3MQ%3D%3D&utm_source=qr",
+  },
+  {
+    image: "./Members/Malay.jpg",
+    name: "Malay Roy",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/malay-roy-14706b307?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/malay_ichigo?igsh=MW93cGkwaTNraGdj",
+  },
+  {
+    image: "./Members/Prasun.jpg",
+    name: "Prasun Biswas",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/prasun-biswas-953874254?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/prasun_ps.07?utm_source=qr&igsh=eGZ2YXZhNXRtc2s5",
+  },
+  {
+    image: "./Members/Radha.jpg",
+    name: "Radha Krishna Pothamsetti",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/radha-krishna-pothamsetti-435649308?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/",
+  },
+  {
+    image: "./Members/Rana.jpg",
+    name: "Rana Dutta",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/rana-dutta-0488b9279?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/_rana_1207?igsh=YjYwY2hmOTBrM2Vp",
+  },
+  {
+    image: "./Members/Rishita.jpg",
+    name: "Rishita Dey",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/rishita-dey-a157622a7?trk=contact-info",
+    instagram: "https://www.instagram.com/itsrishita.09?igsh=bXh0cmtlaDNjNnJn",
+  },
+  {
+    image: "./Members/Snehita.jpg",
+    name: "Snehita Patta",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/snehita-patta-3505a72aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/snehita__patta?utm_source=qr&igsh=MTJjYmhjd3lva2pkMA==",
+  },
+  {
+    image: "./Members/Sohom.jpg",
+    name: "Sohom Mondal",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/sohom-mondal-91597a2b6?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/madx.005?utm_source=qr&igsh=MTQ5aDNsaGtuaHg1OQ==",
+  },
+  {
+    image: "./Members/Suryashish.jpg",
+    name: "Suryashish Saha",
+    post: "Member",
+    linkedin:
+      "https://www.linkedin.com/in/suryashish-saha-09b8862b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:
+      "https://www.instagram.com/suryashish2005?igsh=MWI1YzlrYjEweWl1MA==",
+  },
+  {
+    image: "./Members/Tannistha.jpg",
+    name: "Tannistha Mandal",
+    post: "Member",
+    linkedin: "https://in.linkedin.com/in/tannistha-mandal-54154b309",
+    instagram:
+      "https://www.instagram.com/____.komorebi.____?igsh=MTRycW5jMWd5eTFpbA==",
+  }
+];
